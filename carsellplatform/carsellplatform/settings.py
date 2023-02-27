89fe0581015d896762a84feaca069fb9ff40dfb0
Askarbek Almazbek uulu <almazuulu@gmail.com>
2023-02-27 23:24:16 +0600

    Updated settings

--- carsellplatform/carsellplatform/settings.py
+++ carsellplatform/carsellplatform/settings.py
@@ -35,8 +35,8 @@ AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
+    # # `allauth` specific authentication methods, such as login by e-mail
+    # 'allauth.account.auth_backends.AuthenticationBackend',
    
 ]
 # Application definition
